f8a4e7706130fb1382d630430e6c68d4fb9df6dd
Artem Puhach <artem.puhach@cyberpolice.gov.ua>
2023-01-09 18:00:50 +0200

    06 Как создать плагин сообщений.mp4 05:02

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,23 +1,45 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
             id="email"
             type="text"
+            v-model.trim="email"
+            :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+            class="helper-text invalid"
+            v-if="$v.email.$dirty && !$v.email.required"
+        >Поле емейл не должно быть пустым
+        </small>
+
+        <small
+            class="helper-text invalid"
+            v-else-if="$v.email.$dirty && !$v.email.email"
+        >Пожалуйста введите корректный емайл
+        </small>
       </div>
       <div class="input-field">
         <input
             id="password"
             type="password"
-            class="validate"
+            v-model.trim="password"
+            :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && $v.password.minLength)}"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+            class="helper-text invalid"
+            v-if="$v.password.$dirty && !$v.password.required"
+        >Введіть пароль!</small>
+
+        <small
+            class="helper-text invalid"
+            v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >Мінімамальна довжина пароля {{ $v.password.$params.minLength.min }} символів! Зараз він
+          {{ this.password.length }} </small>
       </div>
       <div class="input-field">
         <input
@@ -30,7 +52,7 @@
       </div>
       <p>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox"/>
           <span>С правилами согласен</span>
         </label>
       </p>
@@ -48,8 +70,42 @@
 
       <p class="center">
         Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        <router-link to="/login">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
+
+
+<script>
+import { email, minLength, required } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'register',
+  data: () => ({
+    email: '',
+    password: '',
+    name: '',
+    agree: false
+  }),
+  validations: {
+    email: {
+      email,
+      required
+    },
+    password: {
+      required,
+      minLength: minLength(8)
+    },
+    name: { required },
+    agree: { checked: v => v }
+  },
+  methods: {
+    submitHandler() {
+      if ( )
+    }
+  }
+}
+
+
+</script>
