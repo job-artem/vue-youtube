0e4e15c58a369c93338ccd6a3fe18878499a6a95
Artem Puhach <artem.puhach@cyberpolice.gov.ua>
2023-01-12 13:30:09 +0200

    07 Добавление Firebase.mp4 - в середине - нужно настроить авторизацию

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 import dateFilter from '@/filters/date.filter'
 import messagePlugin from '@/utils/message.plugin'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 Vue.config.productionTip = false
 
@@ -14,8 +16,25 @@ Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
 Vue.use(Vuelidate)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyBPFpjWRik9Un9Gke-RFq8mL8X3QyzLNDs',
+  authDomain: 'vue-crm-e6991.firebaseapp.com',
+  projectId: 'vue-crm-e6991',
+  storageBucket: 'vue-crm-e6991.appspot.com',
+  messagingSenderId: '837567717504',
+  appId: '1:837567717504:web:cbbecc39c41a9b2e594665',
+  measurementId: 'G-DY7BTL0Y11'
+})
+
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+
+})
+
